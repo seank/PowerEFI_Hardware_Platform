c2342777aedeaa6fec6e511c4c3097179e43779f
Sean Keys <skeys@powerefi.com>
2018-03-01 16:43:17 -0700

    Hardware platform files exported from LiberoSoC
    * Needed inorder to build ISP, Bootloader, and Application.
    
    Change-Id: Ib6a3b11cf2702b0b1c72f73fd03a18cf61c83a1a

---
+++ g4m_system_top_hw_platform.h
@@ -0,0 +1,17 @@
+#ifndef g4m_system_top_HW_PLATFORM_H_
+#define g4m_system_top_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Actel SmartDesign  Mon Jun 17 19:02:22 2013
+*
+*Memory map specification for peripherals in g4m_system_top
+*/
+
+/*-----------------------------------------------------------------------------
+* CM3 subsystem memory map
+* Master(s) for this subsystem: CM3 FABRIC2MSSFIC2 
+*---------------------------------------------------------------------------*/
+#define G4M_SYSTEM_MSS_0                0x40020800U
+
+
+#endif /* g4m_system_top_HW_PLATFORM_H_*/
